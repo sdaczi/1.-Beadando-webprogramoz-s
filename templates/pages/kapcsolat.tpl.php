dfa97261faeef4cd6577d01012f2ad5f120861cf
Daczi Sándor <sdaczi@gmail.com>
2025-04-05 20:27:53 +0200

    commit

---
+++ templates/pages/kapcsolat.tpl.php
@@ -0,0 +1,11 @@
+<h2>Adatok:</h2>
+<p>Ügyvezető: <strong>Valaki Az</strong></p>
+<p>E-mail: <strong>kecskemetivilagjaroklub88@gmail.com</strong></p>
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2725.1232998371565!2d19.700037512361725!3d46.91995773502001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743d91028182be3%3A0xc936b12f3761a2a5!2zVGVybcOpc3pldCBIw6F6YQ!5e0!3m2!1shu!2shu!4v1743875358903!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    <br>
+
+    <a target="_blank" href="https://maps.google.com/?q=46.91995773502001,19.700037512361725">
+  Kattints a nagyobb térképhez
+</a>
