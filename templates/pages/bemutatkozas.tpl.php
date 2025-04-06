ac55db756deccc3c0eafe563907114609c3781ee
Daczi Sándor <sdaczi@gmail.com>
2025-04-06 18:19:02 +0200

    3. rész Daczi Sándor munkája m1btf5

--- templates/pages/bemutatkozas.tpl.php
+++ templates/pages/bemutatkozas.tpl.php
@@ -1,4 +1,34 @@
-<h2>Köszöntés</h2>
-<img src="./images/arc.jpg">
-<h3>Mi a Lorem Ipsum?</h3>
-<p>A mai klub elődje 1988. március 15-én alakult meg</p>
+<h2>Bemutatkozás</h2>
+
+<h3>A Kecskeméti Világjáró Klub rövid története</h3>
+<br><img src="images/.jpg" width="400"><br>
+<p>A mai klub elődje 1988. március 15-én alakult meg a Kecskeméti Planetáriumban. Akkori nevén TIT "Föld
+és Ég" Világjáró Klubnak hívták.
+</p>
+<p>
+Klubvezető titkára Szabó Ödön volt, aki a mai napig elnöke a klubnak. Az
+alapításban segítségére volt Cseh Gábor és Varró László.
+</p>
+<p>
+A klub akkori célkitűzései mostanáig állják az idő próbáját és nem változtak:
+kirándulások, túrák, kalandnyaralások szervezése
+</p>
+<p>
+magas szintű szakmai előadások szervezése
+klubélet megteremtése.
+</p>
+<p>
+Az akkori Világjáró Klub működése 1990-ben félbeszakadt. Varró László az Egyesült Államokban, míg
+Szabó Ödön a Dunántúlon próbált szerencsét és csak 2001-ben tért vissza Kecskemétre
+</p>
+<p>Hamarosan
+újraszervezte a klubbot Kecskeméti Világjáró Klub Természetkutató Egyesület néven. "Újkori" működése
+során helyet biztosított a klubnak
+</p>
+<p>
+a Kecskeméti Planetárium, a Ceremónia Bor- és Rendezvényház,
+jelenleg pedig a Kiskunsági Nemzeti Park Természet Háza.
+</p>
+<p>
+A Kecskeméti Világjáró Klub előadásait minden második kedden 18.00 órától tartja a KNP
+Természet Házában (Kecskemét, Liszt Ferenc u. 19.).  Az előadások megtekintése ingyenes!
